22a8f64b9053a6ff7d76e3b051eb146e745cebfe
Callum Laing <callum.laing91@hotmail.co.uk>
2025-05-20 21:30:31 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Hey!
+
+I've built a simple Weather app to test using the Weather API and see what I could come up with.
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+const apiKey = import.meta.env.WEATHER_API_KEY;
+</script>
+
+<template>
+	<h1>Weather App!</h1>
+</template>
+
+<style scoped></style>
